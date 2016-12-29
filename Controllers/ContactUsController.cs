d40f3e0af5605d241aea5cc02ee9278f73111b1c
satishuk <satishy2k@yahoo.com>
2016-12-29 12:27:55 -0600

    Added missing folders

---
+++ Controllers/ContactUsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFramework.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        // GET: ContactUs
+        public ActionResult Index()
+        {
+	        ViewBag.message = "wel come to Contact page";
+            return View();
+        }
+	   public ActionResult Development()
+	   {
+		  ViewBag.tempMessage = "Enter your comments Development";
+		  return View();
+	   }
+	   [HttpPost]
+	   public ActionResult Development(string taReachout, string emailid, string ssn)
+	   {
+		  ViewBag.tempMessage = "Thanks for submiting the commetns";
+		  return View();
+	   }
+
+	   public ActionResult DefaultDevelopment()
+	   {
+		  ViewBag.tempMessage = "Enter your comments DefaultDevelopment";
+		  return View("Development");
+	   }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+	   public ActionResult Index()
+	   {
+		  ViewBag.Message = "Your application description page.";
+		   
+
+		  return View();
+	   }
+
+	   public ActionResult About()
+	   {
+		  ViewBag.Message = "MY application description page.";
+		  ViewBag.Satish = "Developer";
+
+		  return View();
+	   }
+
+	   public ActionResult Contact()
+	   {
+		  ViewBag.Message = "MY contact page.";
+
+		  return View();
+	   }
+
+	    public ActionResult Laptop()
+	    {
+		    return View("About");
+	    }
+	   public ActionResult Development()
+	   {
+		   ViewBag.tempMessage = "Enter your comments";
+		  return View();
+	   }
+	  [HttpPost]
+	   public ActionResult Development(string taReachout,string emailid, string ssn)
+	   {
+		  ViewBag.tempMessage = "Thanks for submiting the commetns";
+		  return View();
+	   }
+    }
+}
